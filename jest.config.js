1463171ac7b563e94b2e97fb8a67220604fb2530
R4V3YK\Kris <kris.raven@zoho.com>
2021-10-04 22:31:04 +1100

    feat: add Jest unit testing library Includes: - add Jest Types - add Jest config with default values - add Husky to run pre-push hook - add test running to npm Scripts

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	clearMocks: true,
+	restoreMocks: true,
+	resetModules: true,
+	testEnvironment: "node",
+	testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/monorepo-lambda"],
+	coveragePathIgnorePatterns: ["<rootDir>/test/mocks"],
+	collectCoverageFrom: ["event-handlers/**/*.js", "commands/*.js"],
+	coverageReporters: ["json", "lcov", "json-summary", "text"],
+	coverageThreshold: {
+		global: {
+			statements: 85,
+			branches: 85,
+			functions: 85,
+			lines: 85
+		}
+	}
+};
